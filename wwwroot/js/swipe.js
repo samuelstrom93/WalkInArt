e7164ddd5202b1b6f49181b24f78b50534cd3b38
Emiko Johansson <kamonabe0@gmail.com>
2021-01-27 11:48:19 +0100

    Swiper.js

---
+++ wwwroot/js/swipe.js
@@ -0,0 +1,49 @@
+﻿console.log('start.js')
+
+//Karusel
+const swiper = makeSwiper()
+function makeSwiper() {
+    var swiper = new Swiper('.swiper-container', {
+        /*slidesPerView: 4,*/
+        spaceBetween: 30,
+
+        loop: true,
+        speed: 1000,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        breakpoints: {
+            // window width <= 499px
+            499: {
+                slidesPerView: 1,
+                spaceBetweenSlides: 50
+            },
+            // window width <= 999px
+            999: {
+                slidesPerView: 2,
+                spaceBetweenSlides: 50
+            },
+            // window width <= 1199px
+            1199: {
+                slidesPerView: 3,
+                spaceBetweenSlides: 50
+            },
+            // window width <= 1599px
+
+            1599: {
+                slidesPerView: 4,
+                spaceBetweenSlides: 50
+            }
+        }
+
+    });
+    console.log('swiper made')
+
+}
