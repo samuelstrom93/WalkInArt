d3c4de99b1c9cf998ab3512342357eaa0f133ade
Ac.skir@outlook.com <Ac.skir@outlook.com>
2021-01-28 11:17:22 +0100

    Skapat controller, vymodell och vy till utställningssidan "Exhibitions"

---
+++ ViewModels/ExhibitionsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using DSU21_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSU21_2.ViewModels
+{
+    public class ExhibitionsViewModel
+    {
+        public ExhibitionsViewModel(Collection exhibitions)
+        {
+            Exhibitions = exhibitions;
+        }
+        public Collection Exhibitions { get; set; }
+        
+    }
+}
