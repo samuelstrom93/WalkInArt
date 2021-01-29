cf227aed513399d48e7a6c966acbc858376d4dbf
Espen Steinbakk <eksteinbakk@gmail.com>
2021-01-29 09:54:46 +0100

    Removed metod calls in controller and commented the filldbwith data method.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,12 +26,7 @@ namespace DSU21_2.Controllers
 
         public async Task<IActionResult>  Index()
         {
-            //Artist a = await artDbRepo.GetArtistAsync(2);
-            //artDbRepo.AddCollection(a);
-            //artDbRepo.AddArtwork(a.Collections[0]);
-            //List<Tag> tags = await artDbRepo.GetTags();
-            //var collections = await artDbRepo.GetCollectionsWithArt();
-            //Artist artist = await artDbRepo.UpdateArtist(1, "^_^");
+            //Used for filling local DB with data, run once.
             //await artDbRepo.FillDbWithData();
             var test = await artDbRepo.GetCollectionWithTag(1);
             return View();
