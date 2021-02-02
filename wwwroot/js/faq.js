3959ddf740fc6aaba0a26e9a2ad3efd75215aebe
linneame <56341872+linneame@users.noreply.github.com>
2021-02-02 11:01:42 +0100

    ändrat namnen

--- wwwroot/js/faq.js
+++ wwwroot/js/faq.js
@@ -1,1 +1,54 @@
-﻿
+﻿function readMore_1() {
+    var hide = document.getElementById("hide");
+    var readmore1 = document.getElementById("readmore1");
+
+    if (readmore1.style.display === "none") {
+        readmore1.style.display = "inline";
+        hide.style.display = "none";
+    } else {
+        readmore1.style.display = "none";
+        hide.style.display = "inline";
+    }
+}
+
+function readmore_2() {
+    var hide2 = document.getElementById("hide2");
+    var readmore2 = document.getElementById("readmore2");
+
+    if (readmore2.style.display === "none") {
+        readmore2.style.display = "inline";
+        hide2.style.display = "none";
+
+    } else {
+        readmore2.style.display = "none";
+        hide2.style.display = "inline";
+    }
+}
+
+function readmore_3() {
+    var hide3 = document.getElementById("hide3");
+    var readmore3 = document.getElementById("readmore3");
+
+    if (readmore3.style.display === "none") {
+        readmore3.style.display = "inline";
+        hide3.style.display = "none";
+
+    } else {
+        readmore3.style.display = "none";
+        hide3.style.display = "inline";
+    }
+}
+
+function readmore_4() {
+    var hide4 = document.getElementById("hide4");
+    var readmore4 = document.getElementById("readmore4");
+
+    if (readmore4.style.display === "none") {
+        readmore4.style.display = "inline";
+        hide4.style.display = "none";
+
+    } else {
+        readmore4.style.display = "none";
+        hide4.style.display = "inline";
+    }
+}
