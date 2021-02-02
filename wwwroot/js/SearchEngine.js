817c02b905a2f4742e1b84c3f9fdec81881211ce
Philip Bruhn <philip.bruhn@gmail.com>
2021-02-02 11:31:38 +0100

    Lagt till JS som söker artister och kopplat till sökknappen

---
+++ wwwroot/js/SearchEngine.js
@@ -0,0 +1,33 @@
+﻿const searchBar = document.getElementById("search-input")
+const searchButton = document.getElementById("search-btn")
+const searchResponse = document.getElementById("search-response")
+searchButton.addEventListener("click", searchArtists)
+console.log('hi')
+
+ async function searchArtists () {
+    let response = await fetch('https://localhost:44305/api/Artists');
+     let data = await response.json()
+     let arrayLenght = Object.keys(data).length;
+    console.log(data)
+    showSearchResponse(data, arrayLenght)
+}
+//searchArtists().then(data => searchResponse(data));
+
+async function showSearchResponse(data, lenght) {
+    console.log("vi kom hit")
+    //let data = await searchArtists();
+    let html = ""
+    for (let i = 0; i < lenght; i++) {
+        html += `<li>
+                    <p>${data[i].name}</p>
+                 </li>`
+    }
+    console.log(html)
+    searchResponse.innerHTML = html;
+}
+
+
+
+
+
+
