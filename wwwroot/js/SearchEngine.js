e45fe5b6879be2432b09b8dfb8effc2074f6bc2d
Espen Steinbakk <eksteinbakk@gmail.com>
2021-02-03 14:05:57 +0100

    Added search functions for tags and collections.

--- wwwroot/js/SearchEngine.js
+++ wwwroot/js/SearchEngine.js
@@ -1,16 +1,17 @@
 ﻿const searchBar = document.getElementById("search-input")
 const searchButton = document.getElementById("search-btn")
 const searchResponse = document.getElementById("search-suggestions")
-searchButton.addEventListener("click", searchArtists)
+searchButton.addEventListener("click", searchTags)
+searchBar.addEventListener("input", searchCollections)
 console.log(searchResponse)
 
- async function searchArtists () {
-    let response = await fetch('https://localhost:44305/api/Artists');
-     let data = await response.json()
-     let arrayLenght = Object.keys(data).length;
-    console.log(data)
-    showSearchResponse(data, arrayLenght)
-}
+// async function searchArtists () {
+//    let response = await fetch('https://localhost:44305/api/Artists');
+//     let data = await response.json()
+//     let arrayLenght = Object.keys(data).length;
+//    console.log(data)
+//    showSearchResponse(data, arrayLenght)
+//}
 //searchArtists().then(data => searchResponse(data));
 
 async function showSearchResponse(data, lenght) {
@@ -19,6 +20,38 @@ async function showSearchResponse(data, lenght) {
     let html = ""
     for (let i = 0; i < lenght; i++) {
         html += `<option value ="${data[i].name}"></option>`
+        //html += `<option value = "${data[i].name}"><a href="/exhibitions/22"></a></option>`
+    }
+    console.log(html)
+    searchResponse.innerHTML = html;
+}
+
+async function searchCollections() {
+    let link = "https://localhost:44305/api/Collections/";
+        link += searchBar.value;
+    let response = await fetch(link);
+    let data = await response.json()
+    let arrayLenght = Object.keys(data).length;
+    console.log(data)
+    showSearchResponse(data, arrayLenght)
+}
+
+async function searchTags() {
+    let link = "https://localhost:44305/api/Tags/";
+    link += searchBar.value;
+    let response = await fetch(link);
+    let data = await response.json()
+    let arrayLenght = Object.keys(data).length;
+    console.log(data)
+    showTagSearchResponse(data, arrayLenght)
+}
+
+async function showTagSearchResponse(data, lenght) {
+    console.log("vi kom hi")
+    //let data = await searchArtists();
+    let html = ""
+    for (let i = 0; i < lenght; i++) {
+        html += `<option value ="${data[i].Title}"></option>`
         //html += `<option value = "${data[i].name}"><a href="/exhibitions/22"></a></option>`
     }
     console.log(html)
