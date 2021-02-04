262cd562aa148e9267d95a58e2ad6d5eb8032067
Samuel Ström <samuelstrom93@gmail.com>
2021-02-04 14:10:49 +0100

    Fixat in FAQ och lämna utställning

--- wwwroot/js/faq.js
+++ wwwroot/js/faq.js
@@ -1,10 +1,10 @@
 ﻿let answers = document.querySelectorAll('#answer');
 let questions = document.querySelectorAll('#question');
 let arrowIcons = document.querySelectorAll("#arrow");
-
+let questionDivs = document.querySelectorAll('.question-wrapper');
 
 for (let i = 0; i < questions.length; i++) {
-    questions[i].addEventListener('click', function (event) {
+    questionDivs[i].addEventListener('click', function (event) {
         answers[i].style.display = (answers[i].style.display === "block") ? "none" : "block";
         arrowIcons[i].className = (arrowIcons[i].className === "fa fa-chevron-down") ? "fa fa-chevron-up" : "fa fa-chevron-down";
     })
