17b18283787307fc10458f026a57e08c38ecda73
Samuel Ström <samuelstrom93@gmail.com>
2021-02-08 09:56:20 +0100

    För att synliggöra branchen

--- Startup.cs
+++ Startup.cs
@@ -42,6 +42,7 @@ namespace DSU21_2
             });
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
