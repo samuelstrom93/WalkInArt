f57575538e55c9aa1a14f99de74a048095a08878
Samuel Ström <samuelstrom93@gmail.com>
2021-02-11 09:30:58 +0100

    ändrat layout med collapsable

--- wwwroot/js/artist-profile.js
+++ wwwroot/js/artist-profile.js
@@ -1,9 +1,4 @@
-﻿let exhibitionTitles = document.querySelectorAll('.exhibition-title');
-let artworks = document.querySelectorAll(".artworks");
-let arrowIcons = document.querySelectorAll("#arrow");
-let editBtns = document.querySelectorAll(".edit-btn");
-
-// Hanterar lägg till ny utställning
+﻿// Hanterar lägg till ny utställning
 let addExhibitionFormBtn = document.querySelector(".add-exhibition");
 let exhibitionForm = document.querySelector(".exhibition-form");
 
@@ -11,22 +6,6 @@ let exhibitionForm = document.querySelector(".exhibition-form");
 let addObjectFormBtn = document.querySelectorAll(".add-object");
 let objectForm = document.querySelectorAll(".object-form");
 
-
-// Knapptryck på objekttiteln visas alla objekt under den utställningen. Klick igen för att dölja
-for (let i = 0; i < exhibitionTitles.length; i++) {
-    exhibitionTitles[i].addEventListener('click', () => {
-        artworks[i].style.display = (artworks[i].style.display === "block") ? "none" : "block";
-        arrowIcons[i].className = (arrowIcons[i].className === "fa fa-chevron-down") ? "fa fa-chevron-right" : "fa fa-chevron-down";
-    });
-}
-
-// Knapptryck på "Hantera"-knappen visas alla objekt under den utställningen. Klick igen för att dölja
-for (let i = 0; i < editBtns.length; i++) {
-    editBtns[i].addEventListener('click', () => {
-        artworks[i].style.display = (artworks[i].style.display === "block") ? "none" : "block";
-        arrowIcons[i].className = (arrowIcons[i].className === "fa fa-chevron-down") ? "fa fa-chevron-right" : "fa fa-chevron-down";
-    });
-}
 
 // Hanterar nytt utställning
 addExhibitionFormBtn.addEventListener("click", function (event) {
