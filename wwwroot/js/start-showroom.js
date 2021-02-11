012af5bc79757b3b662562ab0919a61d84c40f65
Emiko Johansson <kamonabe0@gmail.com>
2021-02-11 16:04:01 +0100

    Skapat en ny ram för titel som kan förstora beroende på textlength

--- wwwroot/js/start-showroom.js
+++ wwwroot/js/start-showroom.js
@@ -1,5 +1,7 @@
 
 modifyFrames()
+//modifyTitles()
+
 function modifyFrames(){
     let elementsToScaleForBigFrames = document.getElementsByClassName("big-frames")
     let elementsWithImg = document.getElementsByClassName("pictures")
@@ -7,6 +9,16 @@ function modifyFrames(){
 
     for (let i = 0; i < elementsWithImg.length; i++) {
         getMetaForBigFrames(elementsWithImg[i], elementsToScaleForBigFrames[i])
+    }
+}
+
+function modifyTitles() {
+    let elementsToScaleForTitleFrames = document.getElementsByClassName("title-frames")
+    let elementsWithTitle = document.getElementsByClassName("titles")
+    console.log(elementsWithTitle)
+
+    for (let i = 0; i < elementsWithTitle.length; i++) {
+        getMetaFoTitleFrames(elementsWithTitle[i], elementsToScaleForTitleFrames[i])
     }
 }
 
@@ -19,6 +31,14 @@ function getMetaForBigFrames(elementWithImg, elementsToScaleForBigFrames) {
     img.src = elementWithImg.attributes.src.value;
 }
 
+/*function getMetaFoTitleFrames(elementsWithTitle, elementsToScaleForTitle) {
+    var title = elementsWithTitle.textContent;
+    title.onload = function () {
+        let ratio = (this.height / this.width) * 1;
+        elementsToScaleForTitle.attributes.scale.value = `1 ${ratio} 1`
+    };
+    //img.src = elementWithImg.attributes.src.value;
+}*/
 //För att flytta till en annan sida i ett VR rum
 AFRAME.registerComponent('navigate-on-click', {
     schema: {
