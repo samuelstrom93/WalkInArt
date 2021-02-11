b4ffe49a2438fdfb49b8c1f06bf254737de0ddd8
Emiko Johansson <kamonabe0@gmail.com>
2021-02-11 12:57:45 +0100

    Separerat javascript för startsidan

--- wwwroot/js/start-showroom.js
+++ wwwroot/js/start-showroom.js
@@ -1,22 +1,20 @@
 
 modifyFrames()
 function modifyFrames(){
-    let elementsToScale = document.getElementsByClassName("frames")
+    let elementsToScaleForBigFrames = document.getElementsByClassName("big-frames")
     let elementsWithImg = document.getElementsByClassName("pictures")
     console.log(elementsWithImg)
-    for (let i = 0; i  < elementsWithImg.length; i++){
-        getMeta(elementsWithImg[i], elementsToScale[i])
-    }
-    for (let i = elementsWithImg.length; i < elementsToScale.length; i++) {
-        elementsToScale[i].innerHTML = "";
+
+    for (let i = 0; i < elementsWithImg.length; i++) {
+        getMetaForBigFrames(elementsWithImg[i], elementsToScaleForBigFrames[i])
     }
 }
 
-function getMeta(elementWithImg,elementToScale){   
+function getMetaForBigFrames(elementWithImg, elementsToScaleForBigFrames) {
     var img = new Image();
-    img.onload = function(){
-        let ratio =(this.height/this.width)*0.7;
-        elementToScale.attributes.scale.value=`0.7 ${ratio} 0.7`
+    img.onload = function () {
+        let ratio = (this.height / this.width)*1;
+        elementsToScaleForBigFrames.attributes.scale.value = `1 ${ratio} 1`
     };
     img.src = elementWithImg.attributes.src.value;
 }
