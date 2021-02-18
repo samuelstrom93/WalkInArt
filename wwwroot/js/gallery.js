7d4003bf4d93aa3d613f4f41df5bb825139ddadc
Emiko Johansson <kamonabe0@gmail.com>
2021-02-18 09:44:26 +0100

    Kodstädning + lagt summarys innehåll för gallery.js

--- wwwroot/js/gallery.js
+++ wwwroot/js/gallery.js
@@ -1,13 +1,13 @@
 ﻿    /// <summary>
-    /// 
+    /// Används för 2D-rum på utställningssidor
     /// </summary>
+
 let buttons = document.querySelectorAll('.button-overlay');
 let overlays = document.querySelectorAll("#overlay")
 let overlayClose = document.querySelectorAll("#overlay span")
 
 let btnreadmoregallery = document.querySelectorAll('.btn-readmoregallery');
 let info = document.querySelectorAll('.object-info');
-
 
 // Visar mer info om konsten vid knapptryck
 for (let i = 0; i < btnreadmoregallery.length; i++) {
@@ -16,7 +16,7 @@ for (let i = 0; i < btnreadmoregallery.length; i++) {
     })
 }
 
-// �ndrar knappen visa mer och visa mindre p� konstverk vid knapptryck.
+// Ändrar knappen visa mer och visa mindre på konstverk vid knapptryck.
 function showPictureOnly(showhide) {
     if (showhide.value === "Visa info")
         showhide.value = "Visa bild";
@@ -32,7 +32,7 @@ for (let i = 0; i < buttons.length; i++) {
     })
 }
 
-// St�nger overlay genom att klicka p� krysset i overlay eller "mer info"-knappen igen
+// Stänger overlay genom att klicka på krysset i overlay eller "mer info"-knappen igen
 for (let i = 0; i < overlayClose.length; i++) {
     overlayClose[i].addEventListener("click", function (event) {
         overlays[i].style.visibility = (overlays[i].style.visibility == "visible") ? "hidden" : "visible";
@@ -41,11 +41,10 @@ for (let i = 0; i < overlayClose.length; i++) {
     
 }
 
-
 let fbBtn = document.querySelector(".fb-share-button");
 let url = window.location.href;
 
-// S�tter data-href till nuvarande url s� att dela-knappen delar den sidan du �r inne p�
+// Sätter data-href till nuvarande url så att dela-knappen delar den sidan du är inne på
 fbBtn.setAttribute('data-href', url);
 
 
